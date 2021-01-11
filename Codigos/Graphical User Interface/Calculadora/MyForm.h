a424c629f9726efba345a1b07882e214ad5679b2
Edwin Andres Villarraga Ardila <62625858+edanv1401@users.noreply.github.com>
2021-01-11 16:57:33 -0500

    Add files via upload

---
+++ Codigos/Graphical User Interface/Calculadora/MyForm.h
@@ -0,0 +1,200 @@
+#pragma once
+#define CtoDobuble(a) Convert::ToDouble(a)
+#define CtoInt(a) Convert::ToInt16(a)
+#define CtoString(a) Convert::ToString(a)
+namespace GUI {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+
+
+	private:
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18.25F));
+			this->button1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(192)),
+				static_cast<System::Int32>(static_cast<System::Byte>(192)));
+			this->button1->Location = System::Drawing::Point(32, 97);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(76, 53);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"+";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18.25F));
+			this->textBox1->Location = System::Drawing::Point(32, 37);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(164, 35);
+			this->textBox1->TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18.25F));
+			this->textBox2->Location = System::Drawing::Point(304, 37);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(164, 35);
+			this->textBox2->TabIndex = 2;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::Black;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18.25F));
+			this->label1->ForeColor = System::Drawing::Color::White;
+			this->label1->Location = System::Drawing::Point(245, 176);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(27, 29);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"0";
+			// 
+			// button2
+			// 
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18.25F));
+			this->button2->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(192)),
+				static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->button2->Location = System::Drawing::Point(150, 97);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(76, 52);
+			this->button2->TabIndex = 4;
+			this->button2->Text = L"-";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18.25F));
+			this->button3->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->button3->Location = System::Drawing::Point(269, 98);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(76, 52);
+			this->button3->TabIndex = 5;
+			this->button3->Text = L"x";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// button4
+			// 
+			this->button4->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18.25F));
+			this->button4->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(192)));
+			this->button4->Location = System::Drawing::Point(381, 98);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(76, 52);
+			this->button4->TabIndex = 6;
+			this->button4->Text = L"/";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::Black;
+			this->ClientSize = System::Drawing::Size(498, 228);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		int numero1 = CtoInt(textBox1->Text);
+		int numero2 = CtoInt(textBox2->Text);
+		int resultado = numero1 + numero2;
+		label1->Text = CtoString(resultado);
+	}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	int numero1 = CtoInt(textBox1->Text);
+	int numero2 = CtoInt(textBox2->Text);
+	int resultado = numero1 - numero2;
+	label1->Text = CtoString(resultado);
+}
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	int numero1 = CtoInt(textBox1->Text);
+	int numero2 = CtoInt(textBox2->Text);
+	int resultado = numero1 * numero2;
+	label1->Text = CtoString(resultado);
+}
+private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+	Double numero1 = CtoDobuble(textBox1->Text);
+	Double numero2 = CtoDobuble(textBox2->Text);
+	Double resultado = numero1 / numero2;
+	label1->Text = CtoString(resultado);
+}
+};
+}
